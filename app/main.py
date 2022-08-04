3952b606671767738fdffc48174c6a73bb074020
Roy Christian Pery <thercpery@gmail.com>
2022-08-04 15:57:05 +0800

    initial commit

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {
+        "message": "No-Budget Shop API: We'll drain your wallet more by selling second-hand products that you'll think you'll need."
+    }
