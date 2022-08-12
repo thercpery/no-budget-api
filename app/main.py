646e28a619470aa8eaa7e95179c4020ca01bd29f
Roy Christian Pery <thercpery@gmail.com>
2022-08-12 08:49:38 +0800

    changed database from postresql to mongodb and re arranged file structure

--- app/main.py
+++ app/main.py
@@ -6,5 +6,5 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {
-        "message": "No-Budget Shop API: We'll drain your wallet more by selling second-hand products that you'll think you'll need."
+        "message": "No-Budget Shop API: We'll drain your wallet more by selling second-hand products that you don't need."
     }
