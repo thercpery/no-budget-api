efb6deacd134b9604681c7158d09172f5f22675a
Roy Christian Pery <thercpery@gmail.com>
2022-08-26 11:34:18 +0800

    added resend confirmation if expired

--- app/core/services/Confirmations.py
+++ app/core/services/Confirmations.py
@@ -26,13 +26,13 @@ async def send_confirmation_email(user_obj: dict):
                 "Name": user_obj["username"]
             }
         ],
-        "subject": "User confirmation",
+        "subject": "Account confirmation",
         "TextPart": "Greetings! You have to confirm your email by clicking on the link.",
         "HTMLPart": f"""<h3>Hi, {user_obj['username']}!</h3>
 <br>
-<p>You have to confirm your account by clicking on the link <a href='{BASE_URL}/{new_confirmation_data["_id"]}'>here.</a> </p>
+<p>You have to confirm your account by clicking on the link <a href='{BASE_URL}/{new_confirmation_data["_id"]}'>here.</a> Please take note that the link only last for <strong>30 minutes.</strong> </p>
 <br>
-Click on the link below if the link does not work
+Click on the link below if the link does not work.
 {BASE_URL}/{new_confirmation_data["_id"]}
         """
     }
@@ -42,14 +42,16 @@ Click on the link below if the link does not work
 
 async def confirm_user(_id: str):
     confirmation_data = confirmations_collection.find_one({"_id": _id})
+    user_data = users_collection.find_one({"_id": confirmation_data["userId"]})
 
-    if confirmation_data is None:
+    if \
+            (confirmation_data is None) or \
+            (confirmation_data["isConfirmed"]):
         return False
 
     if datetime.utcnow() > datetime.strptime(confirmation_data["expireAt"], "%Y-%m-%dT%H:%M:%S.%f"):
+        await send_confirmation_email(user_obj=user_data)
         return False
-
-    user_data = users_collection.find_one({"_id": confirmation_data["userId"]})
 
     confirmation_data["isConfirmed"] = True
     confirmation_data["dateUpdated"] = datetime.now()
@@ -58,6 +60,22 @@ async def confirm_user(_id: str):
 
     confirmations_collection.update_one({"_id": confirmation_data["_id"]}, {"$set": confirmation_data})
     users_collection.update_one({"_id": user_data["_id"]}, {"$set": user_data})
+
+    message_data = {
+        "to": [
+            {
+                "Email": user_data["email"],
+                "Name": user_data["username"]
+            }
+        ],
+        "subject": "Account successfully confirmed!",
+        "TextPart": "Congrats! You can now shop without any budget whatsoever!",
+        "HTMLPart": f"""<h3>Hi, {user_data['username']}!</h3>
+        <p><strong>Congratulations!</strong> You can now shop without any budget whatsoever!</p>
+        """
+    }
+
+    await Email.send_email(message_data=message_data)
 
     return True
 
