3a7a5262e9b643539fd2d4f4c9068160de9de308
Roy Christian Pery <thercpery@gmail.com>
2022-09-08 09:32:43 +0800

    removed app folder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from .core.middlewares import database
-from .core.routers import Products, Users, Carts, Orders, Admin, Confirmations
+from core.middlewares import database
+from core.routers import Products, Users, Carts, Orders, Admin, Confirmations
 
 app = FastAPI()
 
